836ae3a23e507190d5043c9cb0638cb9cd1480c7
Riko-04 <echoge11@gmail.com>
2025-05-12 14:52:46 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,42 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <h1>This is an {{title}}!</h1>
+      <p>Hello, world!!!</p>
     <router-outlet />
   `,
-  styles: [],
+  styles: [`
+      h1 {
+        padding: 20px;
+        border-radius: 10px;
+        font-size: 30px;
+        color: rgba(87, 91, 191, 0.2);
+        text-align: center;
+        margin: 20px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        transition: transform 0.9s;
+      }
+      h1:hover {
+        transform: scale(1.05);
+      } 
+      h1:focus {
+        outline: none;
+        box-shadow: 0 0 0 4px rgba(0, 123, 255, 0.5);
+        transition: box-shadow 0.3s;
+      }
+      p {
+        background-color: lightblue;
+        padding: 10px;
+        border-radius: 5px;
+        font-size: 20px;
+        color: #333;
+        text-align: center;
+        margin: 20px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        transition: transform 0.9s;
+      }
+    `],
 })
 export class AppComponent {
-  title = 'second-ng-app';
+  title = 'Angular App';
 }
